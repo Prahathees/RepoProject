3ba37b08836114fb727af1e57c49147e311d9a24
Prahathees <prahateesh@gmail.com>
2021-09-25 09:42:07 +0530

    push

---
+++ src/org/add/Greens.java
@@ -0,0 +1,28 @@
+package org.add;
+
+public final class Greens {
+	public final void greensOmr() {
+System.out.println("Greens Tech Solutions");
+}
+	public void greensOmr(int roll) {
+		System.out.println("Greens Tech Solutions "+ roll);
+		}
+	public void greensOmr(float num) {
+		System.out.println("Greens Tech Solutions "+ num);
+		}
+	public void greensOmr(int n,int r) {
+		System.out.println("Greens Tech Solutions "+n+" "+r);
+		}
+	public void greensOmr(String n, long r) {
+		System.out.println(n+" Greens Tech Solutions "+r);
+		}
+public static void main(String[] args) {
+	Greens g = new Greens();
+	g.greensOmr();
+	g.greensOmr(8326863);
+	g.greensOmr(7264.43f);
+	g.greensOmr(23132,214);
+	g.greensOmr("idh", 142987972080l);
+}
+	
+}
---
+++ src/org/add/GreensTech.java
@@ -0,0 +1,23 @@
+package org.add;
+
+public  class GreensTech  {
+	
+
+static String a ;
+
+	public static void greensAdyar() {
+System.out.println("adyar");
+	}
+	public void greensTambaram() {
+System.out.println("Tambaram");
+	}
+	public void greensVelacherry() {
+System.out.println("Velacherry");	}
+	public void greensAnnanagar() {
+System.out.println("annanagar");
+	}
+public static void main(String[] args) {
+	greensAdyar();
+	System.out.println(a);
+}	
+}
